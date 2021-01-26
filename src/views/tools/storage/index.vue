f17a1e9a42c60f099cb352bb4891555461f9dcb7
starao520 <starao520@outlook.com>
2021-01-26 18:01:34 +0800

    添加七牛云

---
+++ src/views/tools/storage/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <!--  <el-tabs v-model="activeName" style="padding-left: 8px;" @tab-click="tabClick">-->
+  <!--&lt;!&ndash;    <el-tab-pane label="本地存储" name="first">&ndash;&gt;-->
+  <!--&lt;!&ndash;      <Local ref="local" />&ndash;&gt;-->
+  <!--&lt;!&ndash;    </el-tab-pane>&ndash;&gt;-->
+  <!--    <el-tab-pane label="七牛云存储" name="second">-->
+  <!--      <QiNiu ref="qiNiu" />-->
+  <!--    </el-tab-pane>-->
+  <!--  </el-tabs>-->
+  <QiNiu ref="qiNiu" />
+</template>
+
+<script>
+import QiNiu from './qiniu/index'
+export default {
+  name: 'Storage',
+  components: { QiNiu },
+  data() {
+    return {
+      activeName: 'first'
+    }
+  },
+  methods: {
+    tabClick(name) {
+      if (this.activeName === 'first') {
+        this.$refs.local.crud.toQuery()
+      } else {
+        this.$refs.qiNiu.crud.toQuery()
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
