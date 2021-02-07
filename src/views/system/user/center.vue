4156f579c4c8f7f783d4c086b87638613f541b73
starao520 <starao520@outlook.com>
2021-02-07 22:33:42 +0800

    无用代码清理

--- src/views/system/user/center.vue
+++ src/views/system/user/center.vue
@@ -38,8 +38,6 @@
       <el-col :xs="24" :sm="24" :md="16" :lg="18" :xl="19">
         <!--    用户资料    -->
         <el-card class="box-card">
-          <!--          <el-tabs v-model="activeName" @tab-click="handleClick">-->
-          <!--            <el-tab-pane label="用户资料" name="first">-->
           <el-form ref="form" :model="form" :rules="rules" style="margin-top: 10px;" size="small" label-width="65px">
             <el-form-item label="昵称" prop="nickName">
               <el-input v-model="form.nickName" style="width: 35%" />
@@ -59,43 +57,6 @@
               <el-button :loading="saveLoading" size="mini" type="primary" @click="doSubmit">保存配置</el-button>
             </el-form-item>
           </el-form>
-          <!--            </el-tab-pane>-->
-          <!--    操作日志    -->
-          <!--            <el-tab-pane label="操作日志" name="second">-->
-          <!--              <el-table v-loading="loading" :data="data" style="width: 100%;">-->
-          <!--                <el-table-column prop="description" label="行为" />-->
-          <!--                <el-table-column prop="requestIp" label="IP" />-->
-          <!--                <el-table-column :show-overflow-tooltip="true" prop="address" label="IP来源" />-->
-          <!--                <el-table-column prop="browser" label="浏览器" />-->
-          <!--                <el-table-column prop="time" label="请求耗时" align="center">-->
-          <!--                  <template slot-scope="scope">-->
-          <!--                    <el-tag v-if="scope.row.time <= 300">{{ scope.row.time }}ms</el-tag>-->
-          <!--                    <el-tag v-else-if="scope.row.time <= 1000" type="warning">{{ scope.row.time }}ms</el-tag>-->
-          <!--                    <el-tag v-else type="danger">{{ scope.row.time }}ms</el-tag>-->
-          <!--                  </template>-->
-          <!--                </el-table-column>-->
-          <!--                <el-table-column-->
-          <!--                  align="right"-->
-          <!--                >-->
-          <!--                  <template slot="header">-->
-          <!--                    <div style="display:inline-block;float: right;cursor: pointer" @click="init">创建日期<i class="el-icon-refresh" style="margin-left: 40px" /></div>-->
-          <!--                  </template>-->
-          <!--                  <template slot-scope="scope">-->
-          <!--                    <span>{{ parseTime(scope.row.createTime) }}</span>-->
-          <!--                  </template>-->
-          <!--                </el-table-column>-->
-          <!--              </el-table>-->
-          <!--              &lt;!&ndash;分页组件&ndash;&gt;-->
-          <!--              <el-pagination-->
-          <!--                :total="total"-->
-          <!--                :current-page="page + 1"-->
-          <!--                style="margin-top: 8px;"-->
-          <!--                layout="total, prev, pager, next, sizes"-->
-          <!--                @size-change="sizeChange"-->
-          <!--                @current-change="pageChange"-->
-          <!--              />-->
-          <!--            </el-tab-pane>-->
-          <!--          </el-tabs>-->
         </el-card>
       </el-col>
     </el-row>
